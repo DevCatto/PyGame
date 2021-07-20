ec41b581c24cb09ebfb75d98e9cef0b2d5e05a6a
DevCatto <87714902+DevCatto@users.noreply.github.com>
2021-07-20 17:57:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('ArenaBattle')
+
+
+x = 200
+y = 200
+
+run = True
+while run:
+
+    for event in pygame.event.get():
+        #quit game
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
